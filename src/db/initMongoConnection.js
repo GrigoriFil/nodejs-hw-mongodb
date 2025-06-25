3131999722052913bdd2f4259b306fe529c2a115
Hryhorii Fil <filgrigorii@gmail.com>
2025-06-25 11:46:44 +0300

    Add debug logs

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,6 +1,13 @@
 import mongoose from 'mongoose';
 
 export const initMongoConnection = async () => {
+  console.log('--- DEBUG INFO ---');
+  console.log('MONGODB_USER:', process.env.MONGODB_USER);
+  console.log('MONGODB_PASSWORD:', process.env.MONGODB_PASSWORD);
+  console.log('MONGODB_URL:', process.env.MONGODB_URL);
+  console.log('MONGODB_DB:', process.env.MONGODB_DB);
+  console.log('--- END DEBUG INFO ---');
+
   const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_URL, MONGODB_DB } =
     process.env;
   const connectionString = `mongodb+srv://<span class="math-inline">\{MONGODB\_USER\}\:</span>{MONGODB_PASSWORD}@<span class="math-inline">\{MONGODB\_URL\}/</span>{MONGODB_DB}?retryWrites=true&w=majority`;
