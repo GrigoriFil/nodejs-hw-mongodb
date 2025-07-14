e3c3f0bbae53d1c4f9d37cd7c45d92adf3819549
Hryhorii Fil <filgrigorii@gmail.com>
2025-07-14 19:55:23 +0300

    fix

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -6,10 +6,37 @@ import {
   updateContactController,
   deleteContactController,
 } from '../controllers/contacts.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { isValidId } from '../middlewares/isValidId.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import {
+  createContactSchema,
+  updateContactSchema,
+} from '../validation/contacts.js';
+import { authenticate } from '../middlewares/authenticate.js';
 
 const router = Router();
 
-router.get('/contacts', getContactsController);
-router.get('/contacts/:contactId', getContactByIdController);
+
+router.use(authenticate);
+
+router.get('/', ctrlWrapper(getContactsController));
+
+router.get('/:contactId', isValidId, ctrlWrapper(getContactByIdController));
+
+router.post(
+  '/',
+  validateBody(createContactSchema),
+  ctrlWrapper(createContactController),
+);
+
+router.patch(
+  '/:contactId',
+  isValidId,
+  validateBody(updateContactSchema),
+  ctrlWrapper(updateContactController),
+);
+
+router.delete('/:contactId', isValidId, ctrlWrapper(deleteContactController));
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -5,28 +5,22 @@ import cookieParser from 'cookie-parser';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import contactsRouter from './routers/contacts.js';
+import authRouter from './routers/auth.js';
 
 export const setupServer = () => {
   const app = express();
+
   app.use(pino());
   app.use(cors());
-
+  app.use(cookieParser());
   app.use(express.json());
 
-  app.use(contactsRouter);
+  app.use('/auth', authRouter);
+  app.use('/contacts', contactsRouter);
 
-  app.use((req, res) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
+  app.use(notFoundHandler);
 
-  app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).json({
-      message: 'An internal server error occurred',
-    });
-  });
+  app.use(errorHandler);
 
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
