6c487deeb38b1acbdeff1c8237c03ef7dcd2b6b9
Hryhorii Fil <75200619+GrigoriFil@users.noreply.github.com>
2025-08-10 13:25:51 +0300

    Fix userId models

--- src/db/models/contact.js
+++ src/db/models/contact.js
@@ -1,34 +1,39 @@
 import { model, Schema } from 'mongoose';
 
 const contactSchema = new Schema(
-  {
-    name: {
-      type: String,
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: false,
+    },
+    isFavourite: {
+      type: Boolean,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      required: true,
+      default: 'personal',
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
       required: true,
     },
-    phoneNumber: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: false,
-    },
-    isFavourite: {
-      type: Boolean,
-      default: false,
-    },
-    contactType: {
-      type: String,
-      enum: ['work', 'home', 'personal'],
-      required: true,
-      default: 'personal',
-    },
-  },
-  {
-    timestamps: true,
-    versionKey: false,
-  },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
 );
 
 export const Contact = model('contact', contactSchema);
